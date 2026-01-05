0f061055bcb3bedfa3520955bfad6ab857296666
mmcgough8 <mwm9490@icloud.com>
2026-01-05 11:55:46 -0500

    "Created Teacher Interface"

---
+++ learnerlabs/src/main/java/rocks/zipcode/learnerlabs/models/Teacher.java
@@ -0,0 +1,9 @@
+package rocks.zipcode.learnerlabs.models;
+
+public interface Teacher {
+    
+    void teach(Learner learner, double numberOfHours);
+    
+    void lecture(Iterable<? extends Learner> learners, double numberOfHours);
+    
+}
