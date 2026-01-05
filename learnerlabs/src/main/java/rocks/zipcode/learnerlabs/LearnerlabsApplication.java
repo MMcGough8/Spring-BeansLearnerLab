8d4515bd906fdd8e1d7c3090f56d15545304ce0d
mmcgough8 <mwm9490@icloud.com>
2026-01-05 11:50:57 -0500

    Initilized spring and created model-person"

---
+++ learnerlabs/src/main/java/rocks/zipcode/learnerlabs/LearnerlabsApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.learnerlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerlabsApplication.class, args);
+	}
+
+}
---
+++ learnerlabs/src/test/java/rocks/zipcode/learnerlabs/LearnerlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.learnerlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
