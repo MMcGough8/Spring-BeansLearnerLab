2aa8fab7144a5d412bcb8b41346ca7545abc6666
mmcgough8 <mwm9490@icloud.com>
2026-01-05 11:54:39 -0500

    "Created Student Class"

---
+++ learnerlabs/src/main/java/rocks/zipcode/learnerlabs/models/Student.java
@@ -0,0 +1,20 @@
+package rocks.zipcode.learnerlabs.models;
+
+public class Student extends Person implements Learner {
+    
+    private double totalStudyTime;
+    
+    public Student(Long id, String name) {
+        super(id, name);
+        this.totalStudyTime = 0;
+    }
+    
+    @Override
+    public void learn(double numberOfHours) {
+        this.totalStudyTime += numberOfHours;
+    }
+    
+    public double getTotalStudyTime() {
+        return totalStudyTime;
+    }
+}
